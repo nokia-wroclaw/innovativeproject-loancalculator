45e4276e33153aadf4d86d09c6f8a0ee9fee5b05
Nat3005 <natalia.rusin.bb@gmail.com>
2022-03-28 19:27:27 +0200

    Create main layout with navbar and routing

---
+++ frontend/src/pages/usefulTerms.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const usefulTerms = () => {
+  return (
+    <div style={{display: 'flex', justifyContent:'center', alignItems: 'center', height: '90vh'}}>
+        <h1>
+            Placeholder for UsefulTerms page
+        </h1>
+    </div>
+  );
+};
+
+export default usefulTerms
