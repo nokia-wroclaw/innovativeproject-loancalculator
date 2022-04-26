27aa29de09e8dc68db9491d3bc86537e828fb6c1
mikulus8922 <mikulus8922@gmail.com>
2022-04-26 23:14:45 +0200

    fix formatting

--- backend/tests/test_app.py
+++ backend/tests/test_app.py
@@ -3,5 +3,11 @@ from backend.app import MortgageCalculator
 
 mortgageCalculator = MortgageCalculator
 
+
 def test_calculate_fixed_rate():
-    assert mortgageCalculator.calculate_fixed_rate(self=mortgageCalculator, credit_amount=100000, loan_term=10, interest_rate=3) == 965.61
+    assert (
+        mortgageCalculator.calculate_fixed_rate(
+            self=mortgageCalculator, credit_amount=100000, loan_term=10, interest_rate=3
+        )
+        == 965.61
+    )
