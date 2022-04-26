7e0697363f6c625fafc8e8d9d255bbb4f408e30f
mikulus8922 <mikulus8922@gmail.com>
2022-04-26 16:39:00 +0200

    fix tests and formatting

--- backend/backend/testrequests.py
+++ backend/backend/testrequests.py
@@ -6,13 +6,13 @@ response = requests.put(
     url=BASE + "mortgageCalculator",
     json={
         "interest_type": "test",
-        "installment_type": "descending",
+        "installment_type": "fixed",
         "mortgage_value": 1,
         "down_payment": 1,
         "credit_amount": 100000,
         "loan_term": 10,
         "interest_rate": 3,
-        "commission": 1
-    }
+        "commission": 1,
+    },
 )
 print(response.json())
