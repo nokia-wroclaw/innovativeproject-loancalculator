521b7c57e68f5631babc3202f688e0542a646f9e
mikulus8922 <mikulus8922@gmail.com>
2022-04-29 20:39:39 +0200

    fix and rename test files, refactor backend files

--- backend/backend/app.py
+++ backend/backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse, abort
+import backend.calculator as calculator
 
 app = Flask(__name__)
 api = Api(app)
@@ -11,7 +12,11 @@ mortgageCalculator_put_args.add_argument(
 )
 # 2
 mortgageCalculator_put_args.add_argument(
-    "installment_type", type=str, help="Installment type is required", required=True
+    "installment_type",
+    type=str,
+    choices=("fixed", "descending"),
+    help='Installment type must be "fixed" or "descending"',
+    required=True,
 )
 # 3
 mortgageCalculator_put_args.add_argument(
@@ -40,51 +45,22 @@ mortgageCalculator_put_args.add_argument(
 
 
 class MortgageCalculator(Resource):
-    MONTHS_IN_YEAR = 12
-
-    def calculate_fixed_rate(self, credit_amount, loan_term, interest_rate):
-        interest_rate = interest_rate / 100
-        loan_term_months = loan_term * self.MONTHS_IN_YEAR
-        payment = (credit_amount * interest_rate) / (
-            self.MONTHS_IN_YEAR
-            * (
-                1
-                - (self.MONTHS_IN_YEAR / (self.MONTHS_IN_YEAR + interest_rate))
-                ** loan_term_months
+    def calculate_mortgage(self, kwargs):
+        if kwargs["installment_type"] == "fixed":
+            base_monthly_payment = calculator.calculate_fixed_rate(
+                kwargs["credit_amount"], kwargs["loan_term"], kwargs["interest_rate"]
             )
-        )
-        base_monthly_payment = round(payment, 2)
-        return base_monthly_payment
-
-    def calculate_descending_rate(self, credit_amount, loan_term, interest_rate):
-        interest_rate = interest_rate / 100
-        base_monthly_payment = []
-        loan_term_months = loan_term * self.MONTHS_IN_YEAR
-        for i in range(loan_term_months):
-            payment = (credit_amount / loan_term_months) * (
-                1 + (loan_term_months - i) * (interest_rate / self.MONTHS_IN_YEAR)
+        elif kwargs["installment_type"] == "descending":
+            base_monthly_payment = calculator.calculate_descending_rate(
+                kwargs["credit_amount"], kwargs["loan_term"], kwargs["interest_rate"]
             )
-            base_monthly_payment.append(round(payment, 2))
-        return base_monthly_payment
-
-    def calculate_mortgage(self, args):
-        if args["installment_type"] == "fixed":
-            base_monthly_payment = self.calculate_fixed_rate(
-                args["credit_amount"], args["loan_term"], args["interest_rate"]
-            )
-        elif args["installment_type"] == "descending":
-            base_monthly_payment = self.calculate_descending_rate(
-                args["credit_amount"], args["loan_term"], args["interest_rate"]
-            )
-        else:
-            abort(404, message="Invalid installment type")
 
         mortgage_serialized = {"monthly_payment": base_monthly_payment}
         return mortgage_serialized
 
     def put(self):
-        args = mortgageCalculator_put_args.parse_args()
-        mortgage = self.calculate_mortgage(args)
+        kwargs = mortgageCalculator_put_args.parse_args()
+        mortgage = self.calculate_mortgage(kwargs)
         return mortgage, 201
 
 
