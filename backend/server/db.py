135b9fb68f4813ad5fa50346bd7922ffcf1c93db
mikulus8922 <mikulus8922@gmail.com>
2022-05-24 15:36:38 +0200

    specify exception types in db class

--- backend/server/db.py
+++ backend/server/db.py
@@ -1,21 +1,24 @@
 from bson.objectid import ObjectId
+import flask_pymongo
 from flask_restful import Api
 from flask_cors import CORS
 from flask_pymongo import PyMongo
 import os
+from pymongo import errors
 
 
 class Db:
     def __init__(self, app):
         self.api = Api(app)
         self.cors = CORS(app, resources={r"/mortgageCalculator/*": {"origins": "*"}})
-        self.mongodb_client = PyMongo(app, uri=os.environ.get("DB_URI"))
+        #self.mongodb_client = PyMongo(app, uri=os.environ.get("DB_URI"))
+        self.mongodb_client = PyMongo(app, uri="mongodb://127.0.0.2:27017/calculator_data")
         self.db = self.mongodb_client.db
 
     def insert_user_logs(self, kwargs):
         try:
             return str(self.db.calculator_logs.insert_one(kwargs).inserted_id)
-        except:
+        except errors.ConnectionFailure:
             print("Could not connect to database")
 
     def get_user_data_by_id(self, user_id):
@@ -25,5 +28,5 @@ class Db:
                 return
             user_data["_id"] = str(user_data["_id"])
             return user_data
-        except:
+        except errors.ConnectionFailure:
             print("Could not connect to database")
